bd7b3956408448fb440e49fc3f987149c3fddf3a
Yu Yin <yxonic@gmail.com>
2018-12-18 21:22:33 +0800

    add all functionality

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,141 @@
 <template>
-  <div>
-    <b-container class="bv-example-row">
-      <b-table striped hover :items="names"></b-table>
-      <!-- main list -->
-      <b-btn v-b-modal.labelingModal>Launch demo modal</b-btn>
-      <b-pagination align="center" size="md" :total-rows="100" :per-page="10">
-      </b-pagination>
-    </b-container>
+  <div @click='loseFocus'>
+    <b-container>
+      <!-- instructions -->
+      <b-jumbotron>
+        <span v-html="instructions"></span>
+      </b-jumbotron>
 
-    <!-- labeling form -->
-    <b-modal id="labelingModal"
-             centered
-             ref="modal"
-             title="Submit your name"
-             size="lg"
-             @ok="handleOk"
-             @shown="initModal">
-      <form @submit.stop.prevent="handleSubmit">
-        <tags-input element-id="tags"
-          ref="tags"
-          :typeahead-max-results=6
-          :only-existing-tags=true
-          :existing-tags="{ 
-              1: 'Web Development',
-              2: 'PHP',
-              3: 'JavaScript',
-              4: 'Python',
-              5: 'Machine Learning',
-              6: '中文',
-              7: '动态规划'
-          }"
-          :typeahead="true"></tags-input>
-        <b-form-input type="text"
-                      ref="input"
-                      placeholder="Enter your name"
-                      v-model="name"></b-form-input>
-      </form>
-    </b-modal>
+      <!-- main list -->
+      <template>
+        <div @click.stop>
+          <b-table :fields="fields" :items="items"
+              @row-clicked="edit">
+            <template v-for="f in fields" :slot="f.key" slot-scope="row">
+              <b-form-select v-if="row.item.edit && f.type == 'class'"
+                  v-model="inputs[f.key]"
+                  :options="f.choices" />
+              <tags-input v-if="row.item.edit && f.type == 'tag'"
+                  v-model="inputs[f.key]"
+                  :existing-tags="f.choices"
+                  :only-existing-tags="true"
+                  :typeahead="true">
+              </tags-input>
+              <div v-if="f.type == 'text'">
+                {{row.item[f.key]}}
+              </div>
+              <div v-if="!row.item.edit && row.item[f.key] && f.type == 'class'">
+                {{f.choices[row.item[f.key]]}}
+              </div>
+              <div v-if="!row.item.edit && row.item[f.key] && f.type == 'tag'">
+                <b-badge v-for="tag in row.item[f.key]"
+                    variant="light" :key="tag">
+                  {{f.choices[tag]}}
+                </b-badge>
+              </div>
+            </template>
+          </b-table>
+        </div>
+      </template>
+
+      <br>
+
+      <!-- pagination -->
+      <b-pagination-nav align="center"
+          size="sm"
+          :limit=10
+          :link-gen="gotoPage"
+          :number-of-pages="nPages"
+          v-model="currentPage" />
+    </b-container>
   </div>
 </template>
 
 <script>
-
 export default {
   data () {
     return {
-      name: '',
-      names: [
-        { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-        { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-        { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-        { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-      ]
+      instructions: '',
+      currentPage: 1,
+      nPages: 1,
+
+      fields: [],
+      items: [],
+      editing: -1,
+      inputs: {}
     }
   },
+
   methods: {
-    initModal () {
-      this.name = ''
-      this.$refs.tags.$refs.taginput.focus()
+    init () {
+      this.axios.get('http://127.0.0.1:5000/oj').then((response) => {
+        this.fields = response.data.fields
+        this.instructions = response.data.instructions || response.data.fields
+        for (var i in this.fields) {
+          window.Vue.set(this.inputs, this.fields[i].key, '')
+        }
+      })
+      this.axios.get('http://127.0.0.1:5000/oj/page/1').then((response) => {
+        this.items = response.data.documents
+        this.nPages = response.data.count
+      })
     },
-    handleOk (evt) {
-      // Prevent modal from closing
-      evt.preventDefault()
-      if (!this.name) {
-        alert('Please enter your name')
-      } else {
-        this.handleSubmit()
+    gotoPage (pageNum) {
+      this.axios.get('http://127.0.0.1:5000/oj/page/' + pageNum).then((response) => {
+        this.items = response.data.documents
+      })
+    },
+    edit (item, index) {
+      if (this.editing < 0) {
+        this.editing = index
+        this.init_editing(index)
+      }
+      if (index != this.editing) {
+          this.save_editing(this.editing)
+          this.items[this.editing].edit = false
+          this.editing = index
+          this.init_editing(index)
+      }
+      window.Vue.set(this.items[this.editing], 'edit', true)
+    },
+    loseFocus () {
+      if (this.editing < 0)
+        return
+      this.save_editing(this.editing)
+      this.items[this.editing].edit = false
+      this.editing = -1
+    },
+    init_editing (index) {
+      for (var i in this.fields) {
+        var field = this.fields[i].key
+        this.inputs[field] = this.items[index][field]
       }
     },
-    handleSubmit () {
-      this.names.push({ isActive: true, age: 38, first_name: 'Duck', last_name: this.name})
-      this.$refs.modal.hide()
+    save_editing (index) {
+      for (var i in this.fields) {
+        var field = this.fields[i].key
+        window.Vue.set(this.items[index], field, this.inputs[field])
+        this.inputs[field] = ''
+      }
+      var id = this.items[index].id, item = {}
+      for (var key in this.items[index]) {
+        if (key == 'id' || key == 'edit')
+          continue
+        item[key] = this.items[index][key]
+      }
+      this.axios.put('http://127.0.0.1:5000/oj/' + id, item).then((response) => {
+        console.log(response)
+      })
     }
+  },
+
+  beforeMount(){
+    this.init()
   }
 }
 </script>
 
 <style lang="sass">
-.tags-input-root
-  height: 80px;
+td
+  max-width: 200px;
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import BootstrapVue from "bootstrap-vue"
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 import '@voerro/vue-tagsinput/dist/style.css'
 
+window.Vue = Vue
 Vue.use(BootstrapVue)
+Vue.use(VueAxios, axios)
 
 import VoerroTagsInput from '@voerro/vue-tagsinput';
 Vue.component('tags-input', VoerroTagsInput);
